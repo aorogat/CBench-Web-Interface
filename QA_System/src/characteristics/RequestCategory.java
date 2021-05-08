0c2fec01fbbaef1730db8f38551ff996b485ca8c
Abdelghny Orogat <53449461+aorogat@users.noreply.github.com>
2021-05-07 23:53:00 -0400

    Delete QA_System directory

--- QA_System/src/characteristics/RequestCategory.java
+++
@@ -1,9 +0,0 @@
-package characteristics;
-
-public class RequestCategory 
-{
-    public static final int ONE_ANSWER = 1;
-    public static final int LIST_OF_ANSWERS = 2;
-    public static final int LIST_OF_UNIQUE_ANSWERS = 3;
-    
-}
--- QA_System/src/characteristics/RequestMultiplicity.java
+++
@@ -1,7 +0,0 @@
-package characteristics;
-
-public class RequestMultiplicity 
-{
-    public static final int ONE_REQUEST = 1;
-    public static final int MULTIPLE_REQUESTS = 2; //(NEW)   Who is the Barak Obamas's wife and how many children he has?
-}
